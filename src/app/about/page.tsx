d200a745df908a95038b54fc0b3fd6d76fb02958
PlasmaStark <leonardo.errati99@gmail.com>
2024-11-27 10:37:34 +0100

    ADD: Navbar primordiale.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
 export default function About() {
-    return (
-      <div>
-        <h1>Chi sono</h1>
-        <p>Mi chiamo [il tuo nome] e sono uno sviluppatore appassionato!</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Chi sono</h1>
+      <p>Mi chiamo [il tuo nome] e sono uno sviluppatore appassionato!</p>
+    </div>
+  );
+}
