4b3bb45067dac3c4700bb88a86245ee1510d7cb3
PlasmaStark <leonardo.errati99@gmail.com>
2025-02-28 17:34:50 +0100

    FIX: unused import

--- src/app/starxive/tags/[tag]/page.tsx
+++ src/app/starxive/tags/[tag]/page.tsx
@@ -15,7 +15,7 @@ export async function generateStaticParams() {
     return tags.map(tag => ({ tag }));
 }
 
-export default async function StarXiveTagPage({ params }: { params: { tag: string } }) {
+export default async function StarXiveTagPage({params}: {params: Promise<{ tag: string }>}) {
     const { tag } = await params;
     const entries: ContentItem[] = await getContentListFromJson(STARXIVE_FILE);
     const filteredEntries = await entries.filter(entry => entry.tags.includes(tag));
