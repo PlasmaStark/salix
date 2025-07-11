13b50012f4bf67a6ed1e8ae5ecc82ad16d297cf2
PlasmaStark <leonardo.errati99@gmail.com>
2025-07-11 21:01:09 +0200

    FIX: starxive

--- config.tsx
+++ config.tsx
@@ -6,4 +6,4 @@ export const TALKS_DIR = path.join(process.cwd(), 'src/contents/talks');
 export const ARTICLE_DIR = path.join(process.cwd(), 'src/contents/primetales');
 export const BIBLIOGRAPHY_DIR = path.join(process.cwd(), 'src/contents/bibliography/references.bib');
 export const WIKI_DIR = path.join(process.cwd(), 'src/contents/encryptopedia');
-export const STARXIVE_FILE = path.join(process.cwd(), 'public/starxive/starxive.json');
+export const STARXIVE_FILE = path.join(process.cwd(), 'src/contents/starxive/starxive.json');
