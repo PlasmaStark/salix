f2bc8de72593f3c0622780100879891c89711c25
PlasmaStark <leonardo.errati99@gmail.com>
2025-11-21 15:21:29 +0100

    FIX: digest template

--- src/app/the-digest/page.tsx
+++ src/app/the-digest/page.tsx
@@ -11,7 +11,7 @@ import upcomingEpisodes from "../../contents/the-digest/upcoming.json";
 export const metadata: Metadata = {
   title: "The Digest",
   description:
-    "Monthly meeting presenting a paper, topic, or concept in 20 minutes.",
+    "Monthly meetings presenting a paper, topic, or concept.",
 };
 
 export default function TheDigestPage() {
@@ -19,9 +19,9 @@ export default function TheDigestPage() {
     <main className="container mx-auto px-2 py-2">
       <Breadcrumb />
 
-      <section className="mb-12 relative w-full h-36 overflow-hidden rounded-lg shadow-2xl">
+      <section className="mb-12 relative w-full h-40 overflow-hidden rounded-lg shadow-2xl">
         <div className="absolute inset-0 bg-accent2 flex flex-col justify-center items-center p-4 z-10">
-          <h1 className="text-5xl font-extrabold text-white tracking-wider uppercase mb-2 drop-shadow-lg">
+          <h1 className="text-5xl font-extrabold text-white tracking-wider text-center uppercase mb-2 drop-shadow-lg">
             THE DIGEST
           </h1>
           <p className="text-normal text-white italic drop-shadow-md text-center">
@@ -32,7 +32,7 @@ export default function TheDigestPage() {
 
       <section className="mb-12">
         <p>
-          <b>The Digest</b> is a monthly summary of advanced research concepts. Here you can find the handouts of all upcoming and past episodes.
+          <b>The Digest</b> is a monthly summary of advanced research concepts. <br></br>Here you can find the handouts of all upcoming and past episodes.
         </p>
       </section>
 
