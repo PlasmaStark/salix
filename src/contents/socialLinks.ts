44d290c326489d3e69ef5c3c8f110625dd1e0dec
Leonardo Errati <64229723+PlasmaStark@users.noreply.github.com>
2026-09-08 12:26:20 +0200

    ADD: orcid

--- src/contents/socialLinks.ts
+++ src/contents/socialLinks.ts
@@ -1,9 +1,10 @@
 import { faBook, faFile} from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faOrcid } from "@fortawesome/free-brands-svg-icons";
 
 export const socialLinks = [
   { href: "https://github.com/PlasmaStark", label: "GitHub", icon: faGithub },
   { href: "https://www.linkedin.com/in/leonardo-errati-76507b213", label: "LinkedIn", icon: faLinkedin },
+  { href: "https://orcid.org/0009-0004-0460-9742", label: "OrcID", icon: faOrcid },
   { href: "/pdfs/cv_eng.pdf", label: "CV", icon: faFile },
   { href: "https://www.goodreads.com/user/show/155458214-leonardus-iii-emperor-of-taured", label: "GoodReads", icon: faBook },
 ];
